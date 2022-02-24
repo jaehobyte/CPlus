d6bcd56ba2db8f741afb116303ab018a9f75394c
jaehomersimpson <p5564989@gmail.com>
2022-02-24 19:30:03 +0900

    FuncTemplate
    FuncTemplate

---
+++ FuncTemplate/FuncTemplate1.cpp
@@ -0,0 +1,19 @@
+﻿#include <iostream>
+using namespace std;
+
+template <typename T>
+T TestFunc(T a)
+{
+    cout << "매개변수 a: " << a << endl;
+    return a;
+}
+int main()
+{
+    cout << "int\t" << TestFunc(3) << endl;
+    cout << "double\t" << TestFunc(3.3) << endl;
+    cout << "char\t" << TestFunc('A') << endl;
+    cout << "char*\t" << TestFunc("TestString") << endl;
+
+    return 0;
+}
+
